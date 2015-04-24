75a543cd859236e805a5334ece9f984b2fc78ffb
Bennett Roesch <meta@spyrosoft.com>
2015-04-24 15:52:19 -0700

    Move On_lists.R to on_lists.R for consistency. Modify files referring to the On_lists.R to reflect this change.

--- R/koans.R
+++ R/koans.R
@@ -5,7 +5,7 @@ koans <- list(
     list(context = "The very basics", title = "Using R as a calculator",  koan = "koans/on_calculations.R"),
     list(context = "The very basics", title = "What is true, is true",  koan = "koans/what_is_true.R"),
     list(context = "Data structures", title = "On vectors",  koan = "koans/on_vectors.R"),
-    list(context = "Data structures", title = "On lists", koan = "koans/On_lists.R"),
+    list(context = "Data structures", title = "On lists", koan = "koans/on_lists.R"),
     list(context = "Subsetting", title = "Vector Subsetting I",  koan = "koans/single_bracket_vector_subsetting.R"),
     list(context = "Functional Programming", title = "Everything is a function",  
          koan = "koans/everything_is_a_function.R"))
